62786173467e0526ab2c3821b5541a8a8a46a7fc
Haliava <grenka388@gmail.com>
2020-07-16 16:32:46 +0400

    cian parser completed

--- cian_parser.py
+++ cian_parser.py
@@ -8,15 +8,22 @@ from bs4 import BeautifulSoup
 
 START_NUM = 210846981
 STOP_NUM = 210847035
+REGEX = r'(>[А-я 0-9,.]+<)+'
+counter = 1
 
 for i in range(START_NUM, STOP_NUM):
     response = requests.get(f'https://izhevsk.cian.ru/sale/suburban/{i}/').content
     soup = BeautifulSoup(response, features='html.parser')
     general_info = [re.sub(r'[A-z0-9<>=" \-\/\n;]', '', str(x))
                     for x in soup.find_all('li', {'data-name': 'FeatureItem'})]
-    area = str(soup.find('li', {'data-name': 'AdditionalFeatureItem'})).split('<span')
-    print(re.match(r'(>[А-я 0-9,.]+<)+', '>10,08 сот.<'))
-    a = re.match(r'(>[А-я 0-9,.]+<)+', area[-1])
-    print(a)
-
-
+    raw_area = str(soup.find('div', {'data-name': 'Description'})).split('<div')[5]
+    area = re.sub(r'^[A-z А-я</> ]', '', str(raw_area[raw_area.find('>'):])).split()[0]
+    raw_price = str(soup.find('span', {'itemprop': 'price'}))
+    price = raw_price.split('"')[1].replace(' ', '')[:-1]
+    with open('cian_result.txt', 'a') as f:
+        try:
+            f.write(f'Площадь:{area} сот.Расстояниедогорода:НЕТ,{price},Инфраструктура:{" ".join(general_info)};\n')
+        except UnicodeEncodeError:
+            print(area, price, general_info)
+        print(f'Номер {counter} записан')
+    counter += 1
