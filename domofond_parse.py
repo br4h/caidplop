587676bf2b69b87478a16493ed9092990fa74b11
domster704 <61056244+domster704@users.noreply.github.com>
2020-07-17 11:07:47 +0400

    new domofond parser

--- domofond_parse.py
+++ domofond_parse.py
@@ -5,9 +5,9 @@ HEADERS = {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.101 YaBrowser/20.7.0.894 Yowser/2.5 Safari/537.36"
 }
 
-MAX_PAGE = 77  # 77
+MAX_PAGE, START_PAGE = 1, 1  # 77
 pages = []
-for i in range(1, MAX_PAGE + 1):
+for i in range(START_PAGE, MAX_PAGE + 1):
     pages.append(
         requests.get(f'https://www.domofond.ru/prodazha-uchastkizemli-izhevsk-c2017?Page={i}', headers=HEADERS)
     )
@@ -17,11 +17,18 @@ data = []
 
 for response in pages:
     soup = bs(response.content, 'html.parser')
-    items = soup.findAll('a', class_='long-item-card__item___ubItG search-results__itemCardNotFirst___3fei6')[1:]
+    items = soup.findAll('a', 'long-item-card__item___ubItG search-results__itemCardNotFirst___3fei6')[1:]
     for el in items:
         response_nested = requests.get(f'https://www.domofond.ru{el["href"]}', headers=HEADERS)
         soup_nested = bs(response_nested.content, 'html.parser')
-        items_nested = soup_nested.findAll('div', class_='detail-information__row___29Fu6')
+        items_nested = soup_nested.findAll('div', 'detail-information__row___29Fu6')
+
+        # ОЦЕНКА РАЙОНА
+        ratings = {}
+        for rating in soup_nested.findAll('div', 'area-rating__row___3y4HH'):
+            ratings[rating.find('div', 'area-rating__label___2Y1bh').get_text(strip=True)] \
+                = rating.find('div', 'area-rating__score___3ERQc').get_text(strip=True)
+
         for el_nested in items_nested:
             params = el_nested.get_text(strip=True).split(':')
             if params[0] == 'Цена' or params[0] == 'Расстояние от центра' or params[0] == 'Площадь':
@@ -33,8 +40,10 @@ for response in pages:
             distance = data[0]['Расстояние от центра']
 
             with open('domofond_parser.txt', 'a', encoding='utf-8') as f:
-                f.write(f'Площадь:{area},Расстояниедогорода:{distance},Цена:{price};\n')
+                f.write(f'Площадь:{area};Расстояниедогорода:{distance};Цена:{price};{";".join([f"{x}: {y}" for x, y in ratings.items()])}\n')
                 data.clear()
+                ratings.clear()
         except IndexError:
             data.clear()
+            ratings.clear()
             continue
