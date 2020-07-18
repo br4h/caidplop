a3ffc19d3bb988dcd70d1d457c6571430970e55a
Haliava <grenka388@gmail.com>
2020-07-18 18:41:45 +0400

    Update Moscow_parser.py

--- Moscow_parser.py
+++ Moscow_parser.py
@@ -1,65 +1,57 @@
-from bs4 import BeautifulSoup as bs
 import requests
-import re
-from fake_useragent import UserAgent
+from bs4 import BeautifulSoup as bs
 
 HEADERS = {
-    'User-Agent': UserAgent(verify_ssl=False).chrome
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
 }
 
 
-def domofond_parser(start_page, end_page, page_counter=True, _headers=None):
-    """Записывает данные из domofond.ru по нескольким параметрам
-
-    start_page(int) -- номер страницы, с которой функция начнёт парсинг
-    end_page(int) -- номер страницы, на которой функция закончит парсинг
-    page_counter(bool) -- показывать номер страницы, которую обрабатывает функция
-    _headers -- user agent
-
-    """
-    if _headers is None:
-        _headers = HEADERS
-
-    url = 'https://www.domofond.ru/prodazha-uchastkizemli-moskovskaya_oblast-r81?Page='
-    counter = 1
+def domofond_parser(start_page, end_page, page_counter=True):
+    pages = []
     for i in range(start_page, end_page + 1):
+        pages.append(
+            requests.get(f'https://www.domofond.ru/prodazha-uchastkizemli-izhevsk-c2017?Page={i}', headers=HEADERS)
+        )
         if page_counter:
-            print(f'СТРАНИЦА НОМЕР {i}:')
+            print(i)
 
-        soup = bs(requests.get(f'{url}{i}', headers=_headers).content, 'html.parser')  # страница
-        articles_links = soup.findAll('a', 'long-item-card__item___ubItG search-results__itemCardNotFirst___3fei6')[1:]
-        for link in articles_links:
-            response_nested = requests.get(f'https://www.domofond.ru{link["href"]}', headers=_headers)  # объявление
+    counter = 1
+    for page in pages:
+        soup = bs(page.content, 'html.parser')
+        items = soup.findAll('a', class_='long-item-card__item___ubItG search-results__itemCardNotFirst___3fei6')[1:]
+        for el in items:
+            try:
+                response_nested = requests.get(f'https://www.domofond.ru{el["href"]}', headers=HEADERS)
+            except requests.exceptions.ConnectionError:
+                continue
             soup_nested = bs(response_nested.content, 'html.parser')
-            detail_information = soup_nested.findAll('div', 'detail-information__row___29Fu6')
+            items_nested = soup_nested.findAll('div', class_='detail-information__row___29Fu6')
 
             # ОЦЕНКА РАЙОНА
             ratings = {}
             try:
                 for rating in soup_nested.findAll('div', 'area-rating__row___3y4HH'):
-                    ratings[rating.find('div', 'area-rating__label___2Y1bh').get_text()] \
-                        = rating.find('div', 'area-rating__score___3ERQc').get_text()
+                    ratings[rating.find('div', 'area-rating__label___2Y1bh').get_text(strip=True)] \
+                        = rating.find('div', 'area-rating__score___3ERQc').get_text(strip=True)
             except AttributeError:
-                print('Оценка отсутствует')
+                # Если
+                print('Фигня')
                 continue
 
-            #  Если нет оценок
-            if not ratings:
+            try:
+                proximity, area, price = [str(items_nested[x]).split('<span>')[-1] for x in range(1, 4)]
+                area = str(area).replace('соток', 'сот')
+                price = price[:price.find("₽") - 1].replace(' ', '')
+            except IndexError:
+                # Если цена в объявлении договорная
+                print('Фигня 2')
                 continue
-
-            area, price = [detail.get_text().split(':')[1] for detail in detail_information[2:4]]
-            proximity = detail_information[1].get_text().split(':')[1].split(',')[0]
-            price = re.sub(r'[₽ ]', '', price)
-            area = re.sub(r'сот..', 'сот', area)
-
-            with open('new_domofond_parser.txt', 'a', encoding='utf-8') as f:
+            with open('../result/domofond_result_new.txt', 'a') as f:
                 f.write(
-                    f'Площадь:{area};Расстояниедогорода:{proximity};Цена:{price};{";".join([f"{x}: {y}" for x, y in ratings.items()])};\n '
+                    f'Площадь: {area[:area.find("<")]};Расстояниедогорода: {proximity[:proximity.find("<")]};Цена: {price};{";".join([f"{x}: {y}" for x, y in ratings.items()])}\n'
                 )
                 print(f'записан номер {counter}')
             counter += 1
-            ratings.clear()
 
 
-if __name__ == "__main__":
-    domofond_parser(1, 1650)  # 1659
+domofond_parser(1, 10)
