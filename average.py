68247c45ee7cb0045a005845d48736047f4eb5ef
domster704 <61056244+domster704@users.noreply.github.com>
2020-07-21 10:52:09 +0400

    get average values

---
+++ average.py
@@ -0,0 +1,18 @@
+def get_average():
+    average = [0 for i in range(9)]
+    amount = 1  # 7268
+    with open('MOSCOW_PARSER.txt', 'r', encoding='utf-8') as f:
+        for i in f:
+            dictionary = list(map(float, [x.split(':')[1].replace(',', '.') for x in i.split(';')[3:-1]]))
+            for j in range(9):
+                average[j] += dictionary[j]
+            amount += 1
+        for i in range(len(average)):
+            average[i] /= amount
+            average[i] = round(average[i], 1)
+    with open('average_values.txt', 'w', encoding='utf-8') as f:
+        f.write(','.join(list(map(str, average))))
+
+
+if __name__ == "__main__":
+    get_average()
