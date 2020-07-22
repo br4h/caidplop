d8f008a5e974088266fa915f95b12c0b7e991ac0
domster704 <61056244+domster704@users.noreply.github.com>
2020-07-22 18:49:31 +0400

    Update design.py

--- design.py
+++ design.py
@@ -2,48 +2,54 @@ import sys, re
 from PyQt5 import uic, QtWidgets
 from PyQt5.QtWidgets import QToolTip
 from PyQt5.QtGui import *
+from PyQt5 import QtCore
 from NeuroLand import domofond_parser
 
 
 #  https://www.domofond.ru/uchastokzemli-na-prodazhu-skoropuskovskiy-1648805439
 
 class UI(QtWidgets.QMainWindow):
-	def __init__(self):
-		super(UI, self).__init__()
-		uic.loadUi('ver2.ui', self)
-		self.link = str()
-		self.data = str()
+    def __init__(self):
+        super(UI, self).__init__()
+        uic.loadUi('ver2.ui', self)
+        self.link = str()
+        self.data = str()
 
-	def get_link(self):
-		try:
-			self.data = domofond_parser.get_data_by_link(self.lineEdit.text())
-			self.data = list([x.split(':')[1].replace(',', '.').replace(' ', '') for x in self.data.split(';')[:-1]])
-			print(self.data)
-			self.lineEdit_4.setText(f'{self.data[0][:-3]}')
-			self.lineEdit_3.setText(f'{self.data[1][:-2]}')
+    def get_link(self):
+        try:
+            self.data = domofond_parser.get_data_by_link(self.lineEdit.text().replace(' ', ''))
+            self.data = list([x.split(':')[1].replace(',', '.').replace(' ', '') for x in self.data.split(';')[:-1]])
+            print(self.data)
+            self.lineEdit_4.setText(f'{self.data[0][:-3]}')
+            self.lineEdit_3.setText(f'{self.data[1][:-2]}')
 
-			self.doubleSpinBox_1.setValue(float(self.data[-2]))  # Транспорт
-			self.doubleSpinBox_2.setValue(float(self.data[3]))  # Экология
-			self.doubleSpinBox_3.setValue(float(self.data[5]))  # ЖКХ
-		except Exception as e:
-			print(e)
+            self.doubleSpinBox_1.setValue(float(self.data[-2]))  # Транспорт
+            self.doubleSpinBox_2.setValue(float(self.data[3]))  # Экология
+            self.doubleSpinBox_3.setValue(float(self.data[5]))  # ЖКХ
+        except Exception as e:
+            print(e)
 
-	# '10сот', '4км', '570000', Эколгоия: 3.1, Чистота: 2.9, ЖКХ: 2.7, Соседи: 3.7,
-	# Условия для детей: 3.5, Спорт и отдых: 3.1, Магазины: 4.2, Транспорт: 2.9, Безопасность: 3.2
-	def initUI(self):
-		self.setFixedSize(800, 600)
-		self.setWindowTitle("NeuroLand")
-		# self.pushButton_4.clicked.connect(lambda: self.get_link())
-		validate = QIntValidator(self)
-		validate.setRange(1, 100000)
-		self.lineEdit_3.setValidator(validate)
-		self.lineEdit_4.setValidator(validate)
-		self.show()
+    def keyPressEvent(self, qKeyEvent):
+        if qKeyEvent.key() == QtCore.Qt.Key_Return:
+            self.get_link()
+
+    # Формат данных в data:
+    # '[10сот', '4км', '570000', Эколгоия: 3.1, Чистота: 2.9, ЖКХ: 2.7, Соседи: 3.7,
+    # Условия для детей: 3.5, Спорт и отдых: 3.1, Магазины: 4.2, Транспорт: 2.9, Безопасность: 3.2]
+    def initUI(self):
+        self.setFixedSize(800, 600)
+        self.setWindowTitle("NeuroLand")
+
+        validate = QIntValidator(self)
+        validate.setRange(1, 2 * 10 ** 9)
+        self.lineEdit_3.setValidator(validate)
+        self.lineEdit_4.setValidator(validate)
+
+        self.show()
 
 
 if __name__ == '__main__':
-	app = QtWidgets.QApplication(sys.argv)
-	root = UI()
-	root.initUI()
-
-	app.exec_()
+    app = QtWidgets.QApplication(sys.argv)
+    root = UI()
+    root.initUI()
+    app.exec_()
