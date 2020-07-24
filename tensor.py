bd455f01ee8f34513b2034b76123a94a08549be4
Haliava <grenka388@gmail.com>
2020-07-24 13:30:56 +0400

    Update tensor.py

--- tensor.py
+++ tensor.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 from additional_data import get_train_data
 from tensorflow import keras
+from tensorflow.keras.callbacks import EarlyStopping
 import numpy as np
 
 # На обучение 13000 данных, на проверку 2000 данных
 (train_data, train_price), (test_data, test_price) = get_train_data(end=13000), get_train_data(start=13000)
-print(test_data)
+
 
 def set_data_from_design(data, price):
     global train_data, train_price
@@ -26,12 +27,16 @@ model.compile(optimizer='adam',
               loss='mse',
               metrics=['accuracy'])
 
+early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=20)
+
 model.fit(x=train_data, y=train_price,
           validation_data=(test_data, test_price),
-          batch_size=128, epochs=500)
+          callbacks=[early_stop],
+          batch_size=128, epochs=400)
 
 test_loss, test_acc = model.evaluate(test_data,  test_price, verbose=2)
-print('\n\n\n')
-print(float(model.predict([[10, 2, 3.5, 3.1, 2.9, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0]])))
 
 print('\nТочность на проверочных данных:', test_acc)
+
+print('\n\n\n')
+print(float(model.predict([[23, 31, 3.0, 3.0, 2.9, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0]])) * 10**9)
