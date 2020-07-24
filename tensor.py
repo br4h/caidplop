32357054214a6cb77e2465f4d639dfe0e4372d31
Haliava <grenka388@gmail.com>
2020-07-24 09:33:49 +0400

    agaIN

--- tensor.py
+++ tensor.py
@@ -3,7 +3,7 @@ from additional_data import get_train_data
 from tensorflow import keras
 
 # На обучение 13000 данных, на проверку 2000 данных
-(train_data, train_price), (test_data, test_price) = get_train_data(), get_train_data(start=13000)
+(train_data, train_price), (test_data, test_price) = get_train_data(end=13000), get_train_data(start=13000)
 
 
 def set_data_from_design(data, price):
@@ -12,9 +12,11 @@ def set_data_from_design(data, price):
 
 
 model = keras.models.Sequential([
-    keras.layers.Dense(12, activation='relu'),
+    keras.layers.Dense(12, activation='relu'),  # или tanh
     keras.layers.Dropout(0.5),
-    keras.layers.Dense(7, activation='relu'),
+    keras.layers.Dense(25, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(10, activation='relu'),
     keras.layers.Dropout(0.5),
     keras.layers.Dense(1)
 ])
@@ -25,7 +27,7 @@ model.compile(optimizer='adam',
 
 model.fit(x=train_data, y=train_price,
           validation_data=(test_data, test_price),
-          batch_size=128, epochs=3000)
+          batch_size=128, epochs=500)
 
 test_loss, test_acc = model.evaluate(test_data,  test_price, verbose=2)
 
